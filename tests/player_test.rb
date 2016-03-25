e1051e4aa49e8c69a4735879a2fe66bd07fb0b9a
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-03-25 09:54:02 -0500

    Added two methods to player test

--- tests/player_test.rb
+++ tests/player_test.rb
@@ -2,5 +2,15 @@ require_relative "test_helper"
 require_relative "../lib/player.rb"
 
 class Player < Minitest::Test
+# name:, wins:, losses:, ties:
+	def test_can_be_created_under_normal_circumstances
+		player = Player.new(name: "Rumpelstiltskin", wins: 3, losses: 1, ties:, 1)
+		refute_nil(player)
+	end
+
+	def test_name_getter
+		player = Player.new(name: "Rumpelstiltskin", wins: 3, losses: 1, ties:, 1)
+		assert_equal("Rumpelstiltskin", player.name)
+	end
 
 end
