e9ccceeb843c1e3587fad772434846f59102b89e
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-03-24 21:33:44 -0500

    Removed unnecessary eslifs

--- lib/basic_rps_game.rb
+++ lib/basic_rps_game.rb
@@ -23,20 +23,22 @@ selection2 = gets.chomp.upcase
 
 def choose_winner(selection1, selection2)
   if selection1 == "rock" && selection2 == "scissors" || 
-  elsif selection1 == "scissors" && selection2 == "paper" || 
-  elsif selection1 == "paper" && selection2 == "rock"
+    selection1 == "scissors" && selection2 == "paper" || 
+    selection1 == "paper" && selection2 == "rock"
     result = "Player 1, WINS!"
 
   elsif selection1 == "scissors" && selection2 == "rock" || 
-  elsif selection1 == "paper" && selection2 == "scissors" || 
-  elsif selection1 == "rock" && selection2 == "paper"
+    selection1 == "paper" && selection2 == "scissors" || 
+    selection1 == "rock" && selection2 == "paper"
     result = "Player 2, WINS!"
 
   elsif selection1 == selection2
     result = "TIE. TRY AGAIN."
   result
+  end
 end
 
+# binding.pry
 
 
 #currently doesn't display any victory messages, just ends after game without saying who won
