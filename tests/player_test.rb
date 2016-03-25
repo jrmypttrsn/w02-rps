bd8687b74e12681e1786627e107ca53aec6cb747
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-03-25 11:14:05 -0500

    Added methods to player test

--- tests/player_test.rb
+++ tests/player_test.rb
@@ -4,13 +4,46 @@ require_relative "../lib/player.rb"
 class Player < Minitest::Test
 # name:, wins:, losses:, ties:
 	def test_can_be_created_under_normal_circumstances
-		player = Player.new(name: "Rumpelstiltskin", wins: 3, losses: 1, ties:, 1)
+		player = Player.new(name: "Rumpelstiltskin", wins: 3, losses: 1, ties: 1)
 		refute_nil(player)
 	end
 
 	def test_name_getter
-		player = Player.new(name: "Rumpelstiltskin", wins: 3, losses: 1, ties:, 1)
+		player = Player.new(name: "Rumpelstiltskin", wins: 3, losses: 1, ties: 1)
 		assert_equal("Rumpelstiltskin", player.name)
 	end
 
+	def test_wins_getter
+    player = Player.new(name: "Rumpelstiltskin", wins: 3, losses: 1, ties: 1)
+    assert_equal(3, dog.rabbits_killed)
+  end
+
+  def test_win_another_round
+    player = Player.new(name: "Rumpelstiltskin", wins: 3, losses: 1, ties: 1)
+    
+    player.win_another_round
+    assert_equal(4, player.wins)
+  end
+
+  def test_lose_another_round
+  	player = Player.new(name: "Rumpelstiltskin", wins: 3, losses: 1, ties: 1)
+    
+    player.win_another_round
+    assert_equal(2, player.losses)
+  end
+
+  def test_tie_another_round
+  	player = Player.new(name: "Rumpelstiltskin", wins: 3, losses: 1, ties: 1)
+
+  	player.tie_another_round
+  	assert_equal(2, player.ties)
+  end
 end
+
+# def test_kill_another_rabbit
+#     dog = Dog.new(name: "Pharaoh", age: 4, species: "Weimaraner", gender: "male", 
+#          tail: "docked", legs: 4, eyes: 2, ears: 2, license: 123456, rabbits_killed: 17)
+    
+#     dog.kill_another_rabbit
+#     assert_equal(18, dog.rabbits_killed)
+#   end
