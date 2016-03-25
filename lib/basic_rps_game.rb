87f1769c2515eb143be4627865c94157b4124e2e
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-03-24 21:52:20 -0500

    Added documentation

--- lib/basic_rps_game.rb
+++ lib/basic_rps_game.rb
@@ -1,18 +1,14 @@
 require 'pry'
 
-# 1. Basic rock-paper-scissors game (no custom objects or classes just yet).
-#    Output should be sensibly formatted. Should only accept valid moves. Players
-#    only need to play a single round. The game should report the result of the
-#    round.
-
-
-# rock > scissors
-# scissors > paper
-# paper > rock
-
 puts "ROCK.PAPER.SCISSORS.\n
 LET'S DO THIS.\n"
 
+# determines a winner based
+#
+# + selection1 refers to the selection player 1 enters
+# + selection2 refers to the selection player 2 enters
+#
+# returns the result based on the rules set up
 
 def choose_winner(selection1, selection2)
   if selection1 == "rock" && selection2 == "scissors" || 
@@ -30,7 +26,13 @@ def choose_winner(selection1, selection2)
   result
   end
 end
-###########################################################
+
+# player makes their selection
+#
+# if the user does not enter a selection a hint will remind them to do so
+#
+# returns the players selection once they've entered a valid selection
+
 def make_selection
   selection = nil
   while selection != "rock" && selection != "paper" && selection != "scissors"
@@ -40,7 +42,7 @@ def make_selection
   selection
 end
 
-# players make their selection by enterin rock, paper, or scissors
+# players make their selection by entering rock, paper, or scissors
 puts "Player 1. Select rock, paper, or scissors."
 selection1 = make_selection
 
@@ -53,6 +55,3 @@ puts result
 
 
 # binding.pry
-
-
-#currently doesn't display any victory messages, just ends after game without saying who won
