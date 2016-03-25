22b9e0dab92e1bc0b34d527e880027a1dd6aeb5c
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-03-25 09:40:40 -0500

    Created player class

---
+++ lib/player.rb
@@ -0,0 +1,35 @@
+require "pry"
+
+# class Dog
+
+# 	attr_accessor :name, :age, :species, :gender, :tail, :legs, :eyes, :ears, :license, :rabbits_killed
+
+# 	def initialize(name:, age:, species:, gender:, tail:, legs:, eyes:, ears:, license:, rabbits_killed:)
+#     @name = name
+#     @age = age
+#     @species = species
+#     @gender = gender
+#     @tail = tail
+#     @legs = legs
+#     @eyes = eyes
+#     @ears = ears
+#     @license = license
+#     @rabbits_killed = rabbits_killed
+#   end
+  
+#   def kill_another_rabbit
+#     @rabbits_killed += 1
+#   end
+# end
+
+class Player
+  attr_reader :name
+  attr_accessor :wins, :losses, :ties
+
+  def initialize(name:, wins:, losses:, ties:)
+    @name = name
+    @wins = wins
+    @losses = losses
+    @ties = ties
+  end
+end
