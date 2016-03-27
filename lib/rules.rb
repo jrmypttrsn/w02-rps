65c4e441e727402dd2d94ae296659454e831a7de
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-03-27 18:04:06 -0500

    added wins a round to player 1 & 2

--- lib/rules.rb
+++ lib/rules.rb
@@ -6,14 +6,16 @@ class Rules
     selection1 == "scissors" && selection2 == "paper" || 
     selection1 == "paper" && selection2 == "rock"
     result = "Player 1, WINS!"
+    @player1.wins_a_round
 
   elsif selection1 == "scissors" && selection2 == "rock" || 
     selection1 == "paper" && selection2 == "scissors" || 
     selection1 == "rock" && selection2 == "paper"
     result = "Player 2, WINS!"
+    @player2.wins_a_round
 
   elsif selection1 == selection2
     result = "TIE. TRY AGAIN."
-  result
   end
+  result
 end
