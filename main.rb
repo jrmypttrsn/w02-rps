1859b7441b2bd730aec0fb7205ca7217c149d117
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-03-27 13:39:53 -0500

    Main now refers to rules instead of game

--- main.rb
+++ main.rb
@@ -1,29 +1,15 @@
 require "pry"
 require_relative "../lib/player.rb"
+require_relative "../lib/rules.rb"
 
-  puts "\n\nROCK.PAPER.SCISSORS.\nLET'S DO THIS.\n"
-
-
-def create_player1
-  print "\nPlayer 1, what is your name? "
+def create_a_player
+  puts "\n\nCREATE A NEW PLAYER\n"
+  print "\nWhat is your? "
   name = gets.chomp
 
-  Player1.new(name: )
+  Player.new(name: name)
 end
 
-def create_player2
-  print "\nPlayer 2, what is your name? "
-  name = gets.chomp
 
-  Player2.new(name: )
-end
 
-def player_stats(player)
-  puts "\n\nPLAYER STATISTICS\n"
-  puts "Player 1's name is #{player1.name}."
-  puts "Player 2's name is #{player2.name}."
-  puts "#{player1.name} has won #{player1.wins} rounds, lost #{player1.losses} rounds, 
-  	and tied #{player1.ties} rounds."
-	puts "#{player2.name} has won #{player2.wins} rounds, lost #{player2.losses} rounds, 
-  	and tied #{player2.ties} rounds."
-end
+puts "\n\nROCK.PAPER.SCISSORS.\nLET'S DO THIS.\n"
