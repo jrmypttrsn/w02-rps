416fbac3deb57d4d9e392182539f8c8083fa50f9
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-03-27 23:01:29 -0500

    Forgot to commit changes to rules

--- lib/rules.rb
+++ lib/rules.rb
@@ -2,20 +2,42 @@ require "pry"
 require_relative "player.rb"
 
 class Rules
-	if selection1 == "rock" && selection2 == "scissors" || 
-    selection1 == "scissors" && selection2 == "paper" || 
-    selection1 == "paper" && selection2 == "rock"
-    result = "Player 1, WINS!"
-    @player1.wins_a_round
+	
+	def initialize(player1:, player2:)
+		@player1 = player1
+		@player2 = player2
+		@valid_weapon = ["rock", "paper", "scissors"]
+	end
 
-  elsif selection1 == "scissors" && selection2 == "rock" || 
-    selection1 == "paper" && selection2 == "scissors" || 
-    selection1 == "rock" && selection2 == "paper"
-    result = "Player 2, WINS!"
-    @player2.wins_a_round
+	def winner_decided	
+		if player1_selection == "rock" && player2_selection == "scissors" || 
+	    player1_selection == "scissors" && player2_selection == "paper" || 
+	    player1_selection == "paper" && player2_selection == "rock"
+	    result = "Player 1, WINS!"
+	    @player1.wins_a_round
 
-  elsif selection1 == selection2
-    result = "TIE. TRY AGAIN."
+	  elsif player1_selection == "scissors" && player2_selection == "rock" || 
+	    player1_selection == "paper" && player2_selection == "scissors" || 
+	    player1_selection == "rock" && player2_selection == "paper"
+	    result = "Player 2, WINS!"
+	    @player2.wins_a_round
+
+	  elsif player1_selection == player2_selection
+	    result = "TIE. TRY AGAIN."
+	  end
+	  result
+	end
+
+	def play_single_round
+		player1_selection = player_choice(@player1)
+		player2_selection = player_choice(@player2)
+		puts winner_decided(player1_selection, player2_selection)
+	end
+
+  def player_chooses_weapon(player)
+	  puts "\n\n#{player.name}, choose your weapon:\n"
+	  @available_weapons.each { |e| puts e}
+	  player.choose_weapon(self)
   end
-  result
+
 end
